04b1c83944ab569f40064455967b40fa126ca4a3
be3oomec <rustikmuzipov@gmail.com>
2022-01-22 21:16:42 +0500

    added consultation section

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,25 @@
+
 // BTN TO TOP
 
-const btnToTop = document.getElementById('btnToTop');
+// const btnToTop = document.getElementById('btnToTop');
 
-const buttonVisibility = () => {
-  if ( document.documentElement.scrollTop <= 150 ) {
-    btnToTop.classList.add('hide');
-  }
-  else {
-    btnToTop.classList.remove('hide');
-    btnToTop.classList.add('visible');
-  }
-};
-buttonVisibility();
+// const buttonVisibility = () => {
+//   if ( document.documentElement.scrollTop <= 150 ) {
+//     btnToTop.classList.add('hide');
+//   }
+//   else {
+//     btnToTop.classList.remove('hide');
+//     btnToTop.classList.add('visible');
+//   }
+// };
+// buttonVisibility();
 
-btnToTop.addEventListener("click", () => {
-  window.scrollTo(0, 0);
-});
+// btnToTop.addEventListener("click", () => {
+//   window.scrollTo(0, 0);
+// });
 
-document.addEventListener('scroll', (e) => {
-  buttonVisibility();
-})
+// document.addEventListener('scroll', (e) => {
+//   buttonVisibility();
+// })
 
 // BTN TO TOP
