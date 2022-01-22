bd1ee2cf708b51938627327a01508fde9545d1cc
be3oomec <rustikmuzipov@gmail.com>
2022-01-22 22:39:50 +0500

    added question section

--- src/js/components/mask.js
+++ src/js/components/mask.js
@@ -4,7 +4,9 @@
 // =========================== \\
 
 const element = document.getElementById('phone');
+const element2 = document.getElementById('phone2');
 const maskOptions = {
   mask: '+{7}(000)000-00-00'
 };
 const mask1 = IMask(element, maskOptions);
+const mask2 = IMask(element2, maskOptions);
