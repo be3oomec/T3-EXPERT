25b2eca7bbd3359eeb92d02b9f3f10ab3edec62b
be3oomec <rustikmuzipov@gmail.com>
2022-01-24 12:24:52 +0500

    scroll to #selector[data-goto]

--- src/js/components/smoothScroll.js
+++ src/js/components/smoothScroll.js
@@ -3,7 +3,7 @@
 // == a add data-goto="selector" == \\
 // ================================ \\
 
-const menuLinks = document.querySelectorAll('.header__link[data-goto]');
+const menuLinks = document.querySelectorAll('a[data-goto]');
 
 if (menuLinks.length > 0) {
   menuLinks.forEach(menuLinks => {
