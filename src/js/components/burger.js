fd84946eb8739f236658dc0165036fb6ebfd11bb
be3oomec <rustikmuzipov@gmail.com>
2022-01-29 12:28:01 +0500

    bootstrap, fixed megamenu

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,14 +1,12 @@
-// ================================== \\
-// ========== MOBILE BURGER ========= \\
-// ================================== \\
+
+// <--------- MOBILE BURGER --------> \\
 
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.header__megamenu');
 
 if (burger) {
   burger.addEventListener('click', function(e) {
-    menu.classList.toggle('active');
-    burger.classList.toggle('burger-active');
-    document.body.classList.toggle('lock');
+    menu.classList.add('active');
+    document.body.classList.add('lock');
   });
 }
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+const btnClose = document.querySelector('.button-close');
+const megaMenu = document.querySelector('.header__megamenu');
+
+if (btnClose) {
+  btnClose.addEventListener('click', () => {
+    menu.classList.remove('active');
+    document.body.classList.remove('lock');
+  })
+}
